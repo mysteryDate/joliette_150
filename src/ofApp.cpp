9debd01b43ffd68c3bd0c9b932c231ae96b7e666
Aaron Krajeski <aaronkrajeski@gmail.com>
2014-10-21 13:33:03 -0400

    Tried out a second window

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,12 +6,18 @@ void ofApp::setup(){
     // For portability
     ofSetDataPathRoot("../Resources/data/");
     
+    controlWindow.setup("Control Window", 50, 50, 1024, 768, false);
+    
     font.loadFont("shimmerbold_opentype.ttf", 30, true, true);
     
     textMessageInput.Create();
     textMessageInput.Bind(7011);
     textMessageInput.SetNonBlocking(true);
-    message = "Hello";
+    Message firstMessage;
+    firstMessage.text = "Hello";
+    firstMessage.position = WINDOW_WIDTH;
+    messages.push_back(firstMessage);
+    text_speed = 1;
     
     toPython.Create();
     toPython.Connect("127.0.0.1", 7012); // Assuming local machine for now
@@ -19,6 +25,8 @@ void ofApp::setup(){
     
     message_trigger = false;
     
+    syphon_out.setName("text output");
+
     controls_on = false;
     
     //    string command = "python "+ofToDataPath("of_controller.py",true);
@@ -33,37 +41,37 @@ void ofApp::update(){
     char textMessage[message_size];
     textMessageInput.Receive(textMessage, message_size);
     
-    int currentTime = ofGetElapsedTimeMillis()/1000;
-    if (currentTime % 3 == 0 and message_trigger and currentTime > 1) {
-        string message = "new message";
-        int success = toPython.Send(message.c_str(), message.length());
-        message_trigger = false;
-    }
-    if (currentTime % 2 == 0 and currentTime % 3 != 0){
-        message_trigger = true;
+    for (int i = 0; i < messages.size(); i++) {
+        messages[i].position -= text_speed;
+        // Get width of the text
+        float width = font.stringWidth(messages[i].text);
+        
+        if (messages[i].position + width < 0) {
+            messages[i].position = WINDOW_WIDTH;
+        }
     }
     
-    if (textMessage[0] != 0) {
-        message = textMessage;
-    }
+//    int currentTime = ofGetElapsedTimeMillis()/1000;
+//    if (currentTime % 3 == 0 and message_trigger and currentTime > 1) {
+//        string message = "new message";
+//        int success = toPython.Send(message.c_str(), message.length());
+//        message_trigger = false;
+//    }
+//    if (currentTime % 2 == 0 and currentTime % 3 != 0){
+//        message_trigger = true;
+//    }
+//    
+//    if (textMessage[0] != 0) {
+//        message = textMessage;
+//    }
     
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    
-    //    int imgWidth = current_picture.width;
-    //    int imgHeight = current_picture.height;
-    //    float widthRatio = WINDOW_WIDTH / imgWidth;
-    //    float heightRatio = WINDOW_HEIGHT / imgHeight;
-    //    if (widthRatio < heightRatio) {
-    //        widthRatio = heightRatio;
-    //    }
-    //    else
-    //        heightRatio = widthRatio;
-    //    current_picture.draw(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
-    font.drawString(message, 50, 50);
-    
+
+    controlWindow.begin();
+    ofClear(0, 0, 0);
     if (controls_on) {
         stringstream reportStream;
         reportStream
@@ -75,8 +83,26 @@ void ofApp::draw(){
         << "    s:      save message database" << endl
         << "    l:      load message database" << endl
         << "    backspace:  shutdown" << endl;
-        ofDrawBitmapString(reportStream.str(), 100,600);
+        ofDrawBitmapString(reportStream.str(), 50,150);
     }
+    controlWindow.end();
+    // Clear with alpha, so we can capture via syphon and composite elsewhere should we want.
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //    int imgWidth = current_picture.width;
+    //    int imgHeight = current_picture.height;
+    //    float widthRatio = WINDOW_WIDTH / imgWidth;
+    //    float heightRatio = WINDOW_HEIGHT / imgHeight;
+    //    if (widthRatio < heightRatio) {
+    //        widthRatio = heightRatio;
+    //    }
+    //    else
+    //        heightRatio = widthRatio;
+    //    current_picture.draw(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
+    font.drawString(messages[0].text, messages[0].position, 50);
+
+    syphon_out.publishScreen();
+
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,9 +2,11 @@
 
 #include "ofMain.h"
 #include "ofxNetwork.h"
+#include "ofxSyphon.h"
+#include "ofxSecondWindow.h"
 
 #define WINDOW_WIDTH 1024
-#define WINDOW_HEIGHT 768
+#define WINDOW_HEIGHT 300
 
 class ofApp : public ofBaseApp{
     
@@ -16,18 +18,29 @@ public:
     void keyPressed(int key);
     void mousePressed(int x, int y, int button);
     
+    ofxSecondWindow controlWindow;
+    
     //    ofImage current_picture;
     //    ofImage next_picture;
     //    int current_picture_index;
     //    ofDirectory picture_directory;
     
     ofTrueTypeFont font;
-    string message;
+    
+    //  A message to be displayed
+    struct Message {
+        string text;
+        int position;
+    };
+    vector<Message> messages;
+    float text_speed;
     
     ofxUDPManager textMessageInput;
     ofxUDPManager toPython;
-    
     bool message_trigger;
+
+    //Syphon
+    ofxSyphonServer syphon_out;
     
     bool controls_on;
 };
